b6aa606c2d14ba919a725368a04b343c012c7c9e
Shane <shanescott86@gmail.com>
2025-01-14 19:48:05 -0600

    implemented wheelpayload and made wheel a tool
    allows for texture and animation editing in editor

--- WHEEL/Scenes/wheel.gd
+++ WHEEL/Scenes/wheel.gd
@@ -1,46 +1,99 @@
-##
-## Hello and welcome to the Persuasion Wheel Game Jam AKA WHEELJAM!
-## This code is free to use and distribute for the purposes of WHEELJAM 
-## and any games you choose to make with it besides. Please provide attribution
-## if you use it outside the jam. :)
-##
+@tool class_name Wheel extends Control
+## Hello and welcome to the Persuasion Wheel Game Jam AKA WHEELJAM! This code
+## is free to use and distribute for the purposes of WHEELJAM and any games
+## you choose to make with it besides. Please provide attribution if you use it
+## outside the jam. :)
 ## This code is also not required for WHEELJAM -- you are free to make your own
 ## implementation of the wheel, use your own assets, etc.
-##
 ## Everything to make the wheel function is in this single script -- this is a 
 ## deliberate choice to keep it plug-n-play for the jam. I don't recommend 
 ## coding like this!
 ## Have fun and enjoy WHEELJAM!
 ## Love, Colin & Shane :)
-##
-class_name Wheel extends Control
 
+#region Export Variables
+@export_category("Wheel Cosmetics")
+@export_group("Size & Scale")
+## a vector2 that specifies the wheel's custom minimum size.
+@export var wheel_size:Vector2 = Vector2(300,300):
+	set(value):
+		wheel_size = value
+		if Engine.is_editor_hint(): self.custom_minimum_size = wheel_size
 
-##
-## Export Variables
-##
+## specifies the wheel's scale.
+@export_range(0,5,0.1) var wheel_scale:float = 1:
+	set(value):
+		wheel_scale = value
+		self.scale.x = wheel_scale
+		self.scale.y = wheel_scale
 
+@export_group("Animations")
+## controls the animation of the rotation of the wheel.
+enum TweenType {
+	TRANS_LINEAR,
+	TRANS_SINE,
+	TRANS_QUINT,
+	TRANS_QUART,
+	TRANS_QUAD,
+	TRANS_EXPO,
+	TRANS_ELASTIC,
+	TRANS_CUBIC,
+	TRANS_CIRC,
+	TRANS_BOUNCE,
+	TRANS_BACK,
+	TRANS_SPRING
+	}
+@export var tween_type:TweenType = TweenType.TRANS_CIRC
+## controls how long the rotation animation will play for.
+@export_range(0,2,0.05) var anim_time= 0.3
+
+@export_group("Textures")
+## an array of slice textures. order is [slice1,slice2,slice3,slice4] (smallest to largest)
+@export var slice_textures:Array[Texture2D] = []:
+	set(value):
+		slice_textures = value
+		if Engine.is_editor_hint():
+			_update_slices_ui(value)
+## the underlay texture.
+@export var underlay_texture:Texture2D = preload("res://the-wheel-godot/WHEEL/Assets/wheel-placeholder/underlay.png"):
+	set(value):
+		underlay_texture = value
+		if Engine.is_editor_hint():
+			_update_underlay_ui(underlay_texture)
+## the overlay texture.
+@export var overlay_texture:Texture2D = preload("res://the-wheel-godot/WHEEL/Assets/wheel-placeholder/overlay.png"):
+	set(value):
+		overlay_texture = value
+		if Engine.is_editor_hint():
+			_update_overlay_ui(overlay_texture)
+## the selector texture.
+@export var selector_texture:Texture2D = preload("res://the-wheel-godot/WHEEL/Assets/wheel-placeholder/selector.png"):
+	set(value):
+		selector_texture = value
+		if Engine.is_editor_hint():
+			_update_selector_ui(selector_texture)
+#endregion
+
+#region Onready Variables
 ## our triangles of varying sizes.
-@onready var slices:Array[Control] = [%slice1,%slice2,%slice3,%slice4] 
+@onready var slices:Array[Control] = [%slice1,%slice2,%slice3,%slice4]
 
 ## our covers to indicate that the selection has already been chosen.
-@onready var covers:Array[Control] = [%cover_up,%cover_down,%cover_left,%cover_right] 
+@onready var covers:Array[Control] = [%cover_up,%cover_down,%cover_left,%cover_right]
 
 ## a reference to our selector node.
-@onready var selector:Control = %selector
+@onready var selector:Control = %wheel_select
 
 ## a reference to our slice gimbal.
 @onready var slice_gimbal:Control = %slice_gimbal
+#endregion
 
-##
-## Internal Variables
-##
-
+#region Internal Variables
 ## enum dictating current state of wheel.
-enum WheelState {AWAITING_SELECTION,ROTATING,NO_INPUT} 
+enum WheelState {AWAITING_SELECTION,ROTATING,NO_INPUT}
 
 ## variable containing current state of wheel.
-var current_wheel_state:WheelState = WheelState.AWAITING_SELECTION 
+var current_wheel_state:WheelState = WheelState.AWAITING_SELECTION
 
 ## where the selector currently is.
 var current_direction:int = 0
@@ -59,16 +112,16 @@ var current_num_selections:int = 0
 ## how many selections are allowed; default is 4.
 var target_selections:int = 4
 
+## 
+var _current_value:WheelPayload
+
 ## rotation value (in degrees) for the wheel directions. [UP,DOWN,LEFT,RIGHT]
 const DIRECTIONS:Array[int] = [0,180,270,90]
+#endregion
 
-
-##
-## Signals
-##
-
+#region Signals
 ## emitted when a new direction is selected.
-signal new_dir_chosen
+#signal new_dir_chosen
 
 ## emitted when the gimbal begins to be rotated.
 signal rotation_started
@@ -78,19 +131,16 @@ signal rotation_finished
 
 ## emitted when the puzzle is complete.
 signal puzzle_finished
+#endregion
 
-##
-## Built-in Functions
-##
-
+#region Built-In Functions
 #called when the scene is loaded into the tree
 func _ready()->void:
 	reset()
-	puzzle_finished.connect(end_check)
+	rotation_finished.connect(end_check)
 
 # handles input for our minigame
-func _unhandled_input(event: InputEvent) -> void:
-	
+func _unhandled_input(_event: InputEvent) -> void:
 	
 	# if up, down, left or right is pressed, process that direction input
 	if Input.is_action_just_pressed("ui_up"):
@@ -113,12 +163,18 @@ func _unhandled_input(event: InputEvent) -> void:
 	# if space is pressed, confirm selection
 	if Input.is_action_just_pressed("ui_accept"):
 		process_confirm_input(current_direction,true)
+#endregion
 
-
-##
-## Custom Functions
-##
-
+#region Custom Functions
+## processes the input direction and moves the selector to that direction.
+func process_direction_input(direction:int,debug=false)->void:
+	for x in current_value_mappings.size():
+		print(current_value_mappings[x])
+	#print debug info if enabled
+	if debug: print("moving selector to "+str(direction)+" degrees"+"\n")
+	
+	#move our selector to the direction
+	selector.rotation_degrees = direction
 
 ## confirms that the current selection has been chosen
 func process_confirm_input(direction:int,debug=false)->void:
@@ -146,15 +202,21 @@ func process_confirm_input(direction:int,debug=false)->void:
 			# rotate the gimbal
 			rotate_slices() 
 
-## processes the input direction and moves the selector to that direction.
-func process_direction_input(direction:int,debug=false)->void:
+## rotates the slice gimbal +90 degrees
+func rotate_slices(debug=false)->void:
+	# if we aren't awaiting a selection, don't rotate
+	if current_wheel_state != WheelState.AWAITING_SELECTION:
+		return
+	# prints debug info if enabled
+	if debug: print("rotating gimbal +90 degrees\n")
 	
-	#print debug info if enabled
-	if debug: print("moving selector to "+str(direction)+" degrees\n")
-	
-	#move our selector to the direction
-	%selector.rotation_degrees = direction
-
+	current_wheel_state = WheelState.ROTATING
+	rotation_started.emit()
+	var tween:Tween = create_tween()
+	# rotates our gimbal using a tween; read tween documentation for controlling the animation.
+	tween.set_trans(int(tween_type))
+	tween.tween_property(%slice_gimbal, "rotation_degrees",%slice_gimbal.rotation_degrees+90,anim_time)
+	tween.finished.connect(end_check)
 
 ## this function resets the minigame.
 func reset()->void:
@@ -163,10 +225,13 @@ func reset()->void:
 	randomize()
 	
 	# remove this if you don't want the selector to reset up every time
-	%selector.rotation_degrees = 0
+	%wheel_select.rotation_degrees = 0
 	
 	# resets gimbal
 	%slice_gimbal.rotation_degrees = 0
+
+	# resets number of selections that have been chosen
+	current_num_selections = 0
 	
 	# hides the covers
 	for x in covers: x.visible = false
@@ -178,17 +243,9 @@ func reset()->void:
 	for x in slices.size():
 		slices[x].rotation_degrees = current_value_mappings[x]
 	
+	_current_value = get_current_wheel_value()
 	# sets the current wheel state to awaiting a selection
 	current_wheel_state = WheelState.AWAITING_SELECTION
-
-## rotates the slice gimbal +90 degrees
-func rotate_slices(debug=false)->void:
-	# prints debug info if enabled
-	if debug: print("rotating gimbal +90 degrees\n")
-	
-	# rotates our gimbal using a tween; read tween documentation for controlling the animation.
-	var tween = get_tree().create_tween()
-	tween.tween_property($slice_gimbal,"rotation_degrees",$slice_gimbal.rotation_degrees+90,0.1)
 
 ## checks if the minigame is finished
 func end_check()->void:
@@ -205,15 +262,61 @@ func end_check()->void:
 ## returns the wheel value
 func get_current_wheel_value()->WheelPayload:
 	var wp = WheelPayload.new()
+	for i in current_value_mappings:
+		if selector.rotation_degrees == i:
+			for j in slices.size():
+				if slices[j].rotation_degrees == j:
+					wp.base_value = 0
+					wp.slice_value = slice_value_multiplier[j]
+					wp.total_value = wp.base_value * wp.slice_value
+				
 	return wp
+#endregion
 
-##
-## Helper Functions
-##
+#region helper functions
+func _update_slices_ui(new_textures:Array[Texture2D])->void:
+	for x in slices.size():
+		slices[x].texture = new_textures[x]
+		slices[x].pivot_offset = Vector2(new_textures[x].get_size().x/2,new_textures[x].get_size().y/2)
+		slices[x].position = Vector2.ZERO
+		slices[x].set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+		slices[x].size = new_textures[x].get_size()
+	for x in covers.size():
+		covers[x].texture = new_textures[3]
+		covers[x].modulate = Color("000000a8")
+		covers[x].pivot_offset = Vector2(Vector2(new_textures[3].get_size().x/2,new_textures[3].get_size().y/2))
+		covers[x].position = Vector2.ZERO
+		covers[x].set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+		covers[x].size = new_textures[3].get_size()
 
-## equivelant to a struct, allows us to create wheel payload object and assign values to wheel.
+func _update_selector_ui(new_texture:Texture2D)->void:
+	var node:=%wheel_select
+	node.texture = new_texture
+	node.pivot_offset = Vector2(new_texture.get_size().x/2,new_texture.get_size().y/2)
+	node.position = Vector2.ZERO
+	node.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+	node.size = new_texture.get_size()
+func _update_overlay_ui(new_texture:Texture2D)->void:
+	var node:=%overlay
+	node.texture = new_texture
+	node.pivot_offset = Vector2(new_texture.get_size().x/2,new_texture.get_size().y/2)
+	node.position = Vector2.ZERO
+	node.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+	node.size = new_texture.get_size()
+func _update_underlay_ui(new_texture:Texture2D)->void:
+	var node:=%underlay
+	node.texture = new_texture
+	node.pivot_offset = Vector2(new_texture.get_size().x/2,new_texture.get_size().y/2)
+	node.position = Vector2.ZERO
+	node.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+	node.size = new_texture.get_size()
+
+#endregion
+
+#region WheelPayload Class
+## allows us to create wheel payload object and assign values to wheel.
 class WheelPayload:
 	var base_value:int
 	var slice_value:int
 	var total_value:int
-	
+#endregion
