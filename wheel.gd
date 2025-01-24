e316019c97c1395077a47d871e426a17d6a2deb7
Shane <shanescott86@gmail.com>
2025-01-24 09:43:29 -0600

    updated documentation

--- wheel.gd
+++ wheel.gd
@@ -1,46 +1,43 @@
 @tool class_name Wheel extends Control
 ## Hello and welcome to the Persuasion Wheel Game Jam AKA WHEELJAM! This code
 ## is free to use and distribute for the purposes of WHEELJAM and any games
-## you choose to make with it besides. Please provide attribution if you use it
-## outside the jam. :)
-## This code is also not required for WHEELJAM -- you are free to make your own
-## implementation of the wheel, use your own assets, etc.
-## Everything to make the wheel function is in this single script -- this is a 
-## deliberate choice to keep it plug-n-play for the jam. I don't recommend 
-## coding like this!
-## Have fun and enjoy WHEELJAM!
-## Love, Colin & Shane :)
+## you choose to make with it outside of wheeljam as well. Please provide 
+## attribution if you use it outside the jam. I'd love to see what you make 
+## with it!
+
+## you can also make your own wheel if you'd like, this one is not required,
+## but I did try to make it as easy to use as possible so people can plug in their
+## own art and sounds and just kinda run with it. This code has been adapted from
+## Colin McInerney's Unity implementation of the wheel.
+
+## Have fun, tag me on bsky (@shanescott.itch.io) if you use the wheel bc again, 
+##I'd LOVE to see what you make, and good luck!
+
+## Love, Shane Scott & Colin McInerney.
 
 #region Signals
-## emitted when a new direction is selected.
-signal new_dir_selected()
-## emitted when a direction is chosen
-signal new_dir_chosen(payload:WheelPayload)
-## emitted when the gimbal begins to be rotated.
-signal rotation_started
-## emitted when the gimbal is finished rotating.
-signal rotation_finished
-## emitted when the puzzle is complete.
-signal puzzle_finished
+signal new_dir_selected() ## emitted when a new direction is selected.
+signal new_dir_chosen(payload:WheelPayload) ## emitted when a direction is chosen
+signal rotation_started ## emitted when the gimbal begins to be rotated.
+signal rotation_finished ## emitted when the gimbal is finished rotating.
+signal puzzle_finished ## emitted when the puzzle is complete.
 #endregion
 
 #region Export Variables
 @export_category("Wheel Cosmetics")
 @export_group("Size & Scale")
-## a vector2 that specifies the wheel's custom minimum size.
-@export var wheel_size:Vector2 = Vector2(300,300):
+@export var wheel_size:Vector2 = Vector2(300,300): ## a vector2 that specifies the wheel's custom minimum size.
 	set(value):
 		wheel_size = value
 		if Engine.is_editor_hint(): self.custom_minimum_size = wheel_size
-## specifies the wheel's scale.
-@export_range(0,5,0.1) var wheel_scale:float = 1:
+@export_range(0,5,0.1) var wheel_scale:float = 1: ## specifies the wheel's scale.
 	set(value):
 		wheel_scale = value
 		self.scale.x = wheel_scale
 		self.scale.y = wheel_scale
 @export_group("Animations")
 ## controls the animation of the rotation of the wheel.
-enum TweenType {
+enum TweenType { 
 	TRANS_LINEAR, ## linear animation.
 	TRANS_SINE, ## sine animation.
 	TRANS_QUINT, ## quint animation.
@@ -54,12 +51,10 @@ enum TweenType {
 	TRANS_BACK, ## back animation.
 	TRANS_SPRING ## spring animation.
 	}
-@export var tween_type:TweenType = TweenType.TRANS_CIRC
-## controls how long the rotation animation will play for.
-@export_range(0,2,0.05) var anim_time= 0.3
+@export var tween_type:TweenType = TweenType.TRANS_CIRC ## holds the value of the animation type enum.
+@export_range(0,2,0.05) var anim_time= 0.3 ## controls how long the rotation animation will play for.
 
 @export_group("Textures")
-
 @export var slice_textures:Array[Texture2D] = []: ## an array of slice textures. order is [slice1,slice2,slice3,slice4] (smallest to largest)
 	set(value):
 		slice_textures = value
@@ -83,37 +78,23 @@ enum TweenType {
 #endregion
 
 #region Onready Variables
-## our triangles of varying sizes.
-@onready var slices:Array[Control] = [%slice1,%slice2,%slice3,%slice4]
-## our covers to indicate that the selection has already been chosen.
-@onready var covers:Array[Control] = [%cover_up,%cover_down,%cover_left,%cover_right]
-## a reference to our selector node.
-@onready var selector:Control = %selector
-## a reference to our slice gimbal.
-@onready var slice_gimbal:Control = %slice_gimbal
+@onready var slices:Array[Control] = [%slice1,%slice2,%slice3,%slice4] ## our triangles of varying sizes.
+@onready var covers:Array[Control] = [%cover_up,%cover_down,%cover_left,%cover_right] ## our covers to indicate that the selection has already been chosen.
+@onready var selector:Control = %selector ## a reference to our selector node.
+@onready var slice_gimbal:Control = %slice_gimbal ## a reference to our slice gimbal.
 #endregion
 
 #region Internal Variables
-## base score values for the slices
-var base_numbers:Array[int] = [-2,-1,1,2]
-## slice value multiplier
-var slice_values:Array[int] = [1,2,3,4]
-## assigns values to directions; format is as follows: [UP,RIGHT,DOWN,LEFT]
-var current_value_mappings:Array[int] = [0,90,180,270]
-## enum dictating current state of wheel.
-enum WheelState {AWAITING_SELECTION,ROTATING,NO_INPUT}
-## variable containing current state of wheel.
-var _state:WheelState = WheelState.AWAITING_SELECTION
-## how many selections have been chosen
-var num_selections:int = 0
-## a WheelPayload object containing the wheel's value
-var _current_value:WheelPayload
-## where the selector currently is.
-var current_direction:int = 0
-## rotation value (in degrees) for the wheel directions. [UP,RIGHT,DOWN,LEFT]
-const DIRECTIONS:Array[int] = [0,90,180,270]
-## how many selections are allowed; default is 4.
-var target_selections:int = 4
+var base_numbers:Array[int] = [-2,-1,1,2] ## base score values for the slices
+var slice_values:Array[int] = [1,2,3,4] ## slice value multiplier
+var current_value_mappings:Array[int] = [0,90,180,270] ## assigns values to directions; format is as follows: [UP,RIGHT,DOWN,LEFT]
+enum WheelState {AWAITING_SELECTION,ROTATING,NO_INPUT} ## enum dictating current state of wheel.
+var _state:WheelState = WheelState.AWAITING_SELECTION ## variable containing current state of wheel.
+var num_selections:int = 0 ## how many selections have been chosen
+var _current_value:WheelPayload ## a WheelPayload object containing the wheel's value
+var current_direction:int = 0 ## where the selector currently is.
+const DIRECTIONS:Array[int] = [0,90,180,270] ## rotation value (in degrees) for the wheel directions. [UP,RIGHT,DOWN,LEFT]
+var target_selections:int = 4 ## how many selections are allowed; default is 4.
 #endregion
 
 #region Built-In Functions
@@ -158,7 +139,7 @@ func process_direction_input(direction:int)->void:
 func process_confirm_input(direction:int)->void:
 	if _state != WheelState.AWAITING_SELECTION: return
 		
-	for x:Control in %covers.get_children(): 
+	for x:Control in %covers.get_children():  # show the covers, increase the num selections, emit the signal, rotate
 		if int(round(x.rotation_degrees)) == direction: 
 			if x.visible: return 
 			
@@ -171,21 +152,21 @@ func process_confirm_input(direction:int)->void:
 func rotate_slices()->void:
 	if _state != WheelState.AWAITING_SELECTION: return
 	
-	_state = WheelState.ROTATING # set the current wheel state to show it is rotating
+	_state = WheelState.ROTATING 
 	current_value_mappings = _rotate_array(current_value_mappings) # +90 to each of our current value mappings
 	_current_value = get_current_wheel_value() # make sure the wheel value is updated
 	rotation_started.emit() 
 	var tween:Tween = create_tween() # create our tween object we will use for the animation
 	tween.set_trans(int(tween_type)) # sets our transition type to our enum
-	tween.tween_property(%slice_gimbal, "rotation_degrees",%slice_gimbal.rotation_degrees+90,anim_time) # rotates gimbal for our specified time in anim time
-	tween.finished.connect(func(): rotation_finished.emit()) # runs the end_check function when the tween is done
+	tween.tween_property(%slice_gimbal, "rotation_degrees",%slice_gimbal.rotation_degrees+90,anim_time) # rotate gimbal
+	tween.finished.connect(func(): rotation_finished.emit()) # emit rotation finished when done anim
 
 ## this function resets the minigame.
 func reset()->void:
 	randomize() # ensures that godot will randomize the shuffle of the mappings
 	selector.rotation_degrees = 0 # remove this if you don't want the selector to reset up every time
-	slice_gimbal.rotation_degrees = 0 # resets gimbal
-	num_selections = 0 # resets number of selections that have been chosen
+	slice_gimbal.rotation_degrees = 0 
+	num_selections = 0 
 	for x:Control in covers: x.visible = false # hides the covers
 
 	current_value_mappings.shuffle() # chooses a random order for our value mappings
@@ -196,8 +177,8 @@ func reset()->void:
 				slice_values[x] = x+1
 
 	base_numbers.shuffle() # shuffles base numbers so random wheel segments = a random base number
-	_current_value = get_current_wheel_value() # sets our current wheel value
-	_state = WheelState.AWAITING_SELECTION # sets the current wheel state to awaiting a selection
+	_current_value = get_current_wheel_value() 
+	_state = WheelState.AWAITING_SELECTION 
 
 ## checks if the minigame is finished
 func end_check()->void:
@@ -257,7 +238,7 @@ func _update_node_ui(node:Control,new_texture:Texture2D)->void:
 #endregion
 
 #region WheelPayload Class
-## allows us to create wheel payload object and assign values to wheel.
+## allows us to create wheel payload objects and assign values to the wheel.
 class WheelPayload:
 	var base_value:int
 	var slice_value:int
